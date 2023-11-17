5d2b8e9424c4f276a6c4fe75455a3944f1b695b5
zanty12 <54949055+zanty12@users.noreply.github.com>
2023-11-17 14:06:13 +0900

    map関連の実装 (#21)

--- cell.h
+++ cell.h
@@ -21,7 +21,7 @@ class Cell : public GameObject
 	//特にやることない
 public:
 	Cell() = delete;
-	Cell(int x, int y, int tex) : GameObject(Vector2(x* GameObject::size_, y* GameObject::size_), 0.0f, tex) {}
+	Cell(int x, int y, int tex) : GameObject(Vector2(x* GameObject::size_ + GameObject::size_ / 2, y* GameObject::size_ + GameObject::size_ / 2), 0.0f, tex) {} //ゲーム座標からマス座標に変換
 	virtual ~Cell() = default;
 	void Update() override {}
 	void Draw() override {}
