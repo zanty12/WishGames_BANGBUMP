eea911de135b4055254a6c4310c5145f311701c0
Shunn0000 <149126392+Shunn0000@users.noreply.github.com>
2023-11-17 14:32:45 +0900

    Scene mngr (#22)
    Co-authored-by: zanty12 <54949055+zanty12@users.noreply.github.com>

--- Game.h
+++ Game.h
@@ -1,1 +1,17 @@
 #pragma once
+#include "Scene.h"
+#include "player.h"
+#include "mapmngr.h"
+
+class Game :public Scene
+{
+private:
+	Player* player_;
+	MapMngr* mapmngr_;
+
+public:
+
+	void Update()override{}
+	void Draw()override{}
+
+};
--- Title.h
+++ Title.h
@@ -1,1 +1,21 @@
 #pragma once
+#include "Scene.h"
+
+class Title :public Scene
+{
+public:
+	Title();
+	~Title();
+
+	void Update()override {}
+
+	void Draw()override {}
+};
+
+Title::Title()
+{
+}
+
+Title::~Title()
+{
+}
