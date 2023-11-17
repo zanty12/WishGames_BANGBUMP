d06a509b2e50d584e3b0b5fa267b7acde13282a0
A1 <57588635+ao-nea-one@users.noreply.github.com>
2023-11-17 13:18:51 +0900

    Wind制作途中 (#20)
    Co-authored-by: zanty12 <54949055+zanty12@users.noreply.github.com>

--- wind.h
+++ wind.h
@@ -1,1 +1,16 @@
 #pragma once
+#include "attribute.h"
+
+class Wind : public Attribute {
+private:
+	float power_ = 0.0f;
+	const float maxPower_ = 5.0f;
+	const float rotInputJudgeMin = 1.0f;	// ‚Ü‚í‚·‚ð”»’è‚·‚é
+	const float friction_ = 0.95f;			// –€ŽC
+
+public:
+	Wind(Player& player) : Attribute(player) { }
+
+	Vector2 Move(void) override;
+	void Action(void) override;
+};
