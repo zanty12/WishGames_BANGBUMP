9ea6ae3acf4dedda3bc6f521dcf954a543ecbabc
高沢駿矢 THS22 <ths20908@ths.hal.ac.jp>
2023-11-17 13:09:54 +0900

    Prep更新

--- Prep.h
+++ Prep.h
@@ -1,1 +1,15 @@
 #pragma once
+#include "Scene.h"
+#include "player.h"
+
+class Prep :public Scene
+{
+private:
+	Player* player_;
+
+public:
+
+	void Update()override{}
+	void Draw()override{}
+
+};
