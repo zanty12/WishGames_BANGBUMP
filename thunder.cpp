c8cb0a3b52d415f43eabb644faeca552253a25f8
Runbo555 <148732791+Runbo555@users.noreply.github.com>
2023-11-29 13:45:06 +0900

    Thunder作成 (#38)
    アクションはまだ
    moveは完成
    
    ---------
    
    Co-authored-by: zanty12 <54949055+zanty12@users.noreply.github.com>

---
+++ thunder.cpp
@@ -0,0 +1,29 @@
+//--------------------------------------------------------------------------------
+// 
+// サンダー[thunder.cpp]
+// 
+// 作成者 趙潤博
+// 
+// 最終更新日	2023/11/22
+// 
+//--------------------------------------------------------------------------------
+#include "thunder.h"
+#include"xinput.h"
+#include"lib/collider2d.h"
+
+Vector2 Thunder::Move()
+{
+	Vector2 vel = player_.GetVel();
+	Vector2 stick = Input::GetStickLeft(0);
+	Vector2 pstick = Input::GetPreviousStickLeft(0);
+
+	if (Input::GetPreviousStickLeft(0) != Vector2(0, 0) && Input::GetStickLeft(0) == Vector2(0, 0))
+	{
+		player_.SetVel(-stick * 10);
+	}
+}
+
+void Thunder::Action()
+{
+
+}
