fe19e00357a08fa67c1a75e58bf29ea6fd9110e5
hamayamaeiichi <ahamayama13@gmail.com>
2023-12-01 10:19:20 +0900

    Collider2D描画(とりま円のみ) DrawLineの追加

--- sprite.h
+++ sprite.h
@@ -22,6 +22,7 @@
 #include "lib\dxlib.h"
 #include "texture.h"
 #include "graphical.h"
+#include "lib/collider2d.h"
 
 /*------------------------------------------------------------------------------
 * defineêÈåæ
@@ -49,6 +50,9 @@
 void InitSprite(void);
 void DrawSprite(int texNo, Vector2 pos, float rot, Vector2 scale, Color color);
 void ReleaseSprite(void);
+
+void DrawLine(Vector2 startPosition, Vector2 endPosition, Color color, float width = 10);
+void DrawCollider(PHYSICS::Vertex1 vertex, Color color);
 
 void ViewUpdate(Vector2 pos);
 void ProjectionUpdate(int screenWidth, int screenHeight);
