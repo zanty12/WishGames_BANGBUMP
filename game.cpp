552e58c6c29cce7ed2ce3720b5e892136e6a50d6
zant123456 <davidcth123456@gmail.com>
2023-12-01 11:15:56 +0900

    衝突をscale反映

--- game.cpp
+++ game.cpp
@@ -6,16 +6,22 @@
 #include "thunder.h"
 #include "wind.h"
 
-Game::Game() {
-    mapmngr_ = new MapMngr("data/map/mariomaker.csv",this);
+Game::Game()
+{
+    mapmngr_ = new MapMngr("data/map/mariomaker.csv", this);
     player_ = new Player(mapmngr_->GetPlayerSpawn(), 0.0f, LoadTexture("player.jpg"), Vector2(0.0f, 0.0f), mapmngr_);
+    player_->SetScale(Vector2(player_->GetScale().x, player_->GetScale().y * 2));
+    player_->SetPos(Vector2(player_->GetPos().x, player_->GetPos().y + player_->GetScale().y / 2));
 }
 
-void Game::Update() {
+void Game::Update()
+{
     mapmngr_->Update();
     player_->Update();
 }
-void Game::Draw(){
+
+void Game::Draw()
+{
     mapmngr_->Draw();
     player_->Draw();
 }
@@ -26,41 +32,49 @@ void Game::DebugMenu()
     ImGui::Text(u8"プレイヤー座標");
     ImGui::Text("x:%f, y: %f", player_->GetPos().x, player_->GetPos().y);
     static std::string preview = u8"無属性";
-    if(ImGui::BeginCombo(u8"プレイヤー移動属性", preview.c_str()))
+    if (ImGui::BeginCombo(u8"プレイヤー移動属性", preview.c_str()))
     {
-        if (ImGui::Selectable("Fire", false)) {
+        if (ImGui::Selectable("Fire", false))
+        {
             preview = "Fire";
             player_->SetAttribute(new Fire(player_));
         }
-        if (ImGui::Selectable("Wind", false)) {
+        if (ImGui::Selectable("Wind", false))
+        {
             preview = "Wind";
             player_->SetAttribute(new Wind(player_));
         }
-        if (ImGui::Selectable("Thunder", false)) {
+        if (ImGui::Selectable("Thunder", false))
+        {
             preview = "Thunder";
             player_->SetAttribute(new Thunder(player_));
         }
-        if (ImGui::Selectable("Dark", false)) {
+        if (ImGui::Selectable("Dark", false))
+        {
             preview = "Dark";
             player_->SetAttribute(new Dark(player_));
         }
         ImGui::EndCombo();
     }
-    if(ImGui::BeginCombo(u8"プレイヤー攻撃属性", preview.c_str()))
+    if (ImGui::BeginCombo(u8"プレイヤー攻撃属性", preview.c_str()))
     {
-        if (ImGui::Selectable("Fire", false)) {
+        if (ImGui::Selectable("Fire", false))
+        {
             preview = "Fire";
             player_->SetAttackAttribute(new Fire(player_));
         }
-        if (ImGui::Selectable("Wind", false)) {
+        if (ImGui::Selectable("Wind", false))
+        {
             preview = "Wind";
             player_->SetAttackAttribute(new Wind(player_));
         }
-        if (ImGui::Selectable("Thunder", false)) {
+        if (ImGui::Selectable("Thunder", false))
+        {
             preview = "Thunder";
             player_->SetAttackAttribute(new Thunder(player_));
         }
-        if (ImGui::Selectable("Dark", false)) {
+        if (ImGui::Selectable("Dark", false))
+        {
             preview = "Dark";
             player_->SetAttackAttribute(new Dark(player_));
         }
