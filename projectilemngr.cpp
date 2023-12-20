4b25923895d9c21963c318be47e256fb4f446c1b
Cheung To Hung <54949055+zant123456@users.noreply.github.com>
2023-12-20 20:04:26 +0900

    projectilemngr追加

---
+++ projectilemngr.cpp
@@ -0,0 +1,18 @@
+﻿#include "projectilemngr.h"
+
+void ProjectileMngr::Update()
+{
+    for(auto projectile : projectiles_)
+    {
+        if(projectile == nullptr)
+            continue;
+
+        projectile->Update();
+
+        if(projectile->GetDiscard())
+        {
+            delete projectile;
+            projectile = nullptr;
+        }
+    }
+}
---
+++ projectilemngr.h
@@ -0,0 +1,25 @@
+﻿#pragma once
+#include "movableobj.h"
+
+class ProjectileMngr
+{
+private:
+    std::list<MovableObj*> projectiles_;
+public:
+    ProjectileMngr() = default;
+    ~ProjectileMngr()
+    {
+        for(MovableObj* projectile : projectiles_)
+        {
+            delete projectile;
+        }
+        projectiles_.clear();
+    }
+    void Update();
+    void Add(MovableObj* projectile)
+    {
+        if(projectile != nullptr)
+            projectiles_.push_back(projectile);
+    }
+    std::list<MovableObj*> GetProjectiles() const { return projectiles_; }
+};
