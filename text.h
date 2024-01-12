0fbbe07705168d91314468fbadd5e0ee814fdbb9
zanty12 <54949055+zanty12@users.noreply.github.com>
2024-01-12 10:57:25 +0900

    Single game (#123)
    Co-authored-by: Cheung To Hung <54949055+zant123456@users.noreply.github.com>

--- text.h
+++ text.h
@@ -1,4 +1,6 @@
 ﻿#pragma once
+#pragma comment(lib,"d2d1.lib")
+#pragma comment(lib,"dwrite.lib")
 #include "graphical.h"
 #include <dwrite.h>
 #include <d2d1.h>
@@ -8,16 +10,15 @@ class Text
 {
 private:
     //directwrite
-    static IDWriteFactory* pDWriteFactory_ = nullptr;
-    static IDWriteTextFormat* pTextFormat_ = nullptr;
+    static IDWriteFactory* pDWriteFactory_;
+    static IDWriteTextFormat* pTextFormat_;
     //D2D
-    static ID2D1Factory* pD2DFactory_ = nullptr;
-    static ID2D1RenderTarget* pRT_ = nullptr;
-    static ID2D1SolidColorBrush* pSolidBrush_ = nullptr;
-    static IDXGISurface* pBackBuffer_ = nullptr;
-
+    static ID2D1Factory* pD2DFactory_;
+    static ID2D1RenderTarget* pRT_;
+    static ID2D1SolidColorBrush* pSolidBrush_;
+    static IDXGISurface* pBackBuffer_;
     //text formats
-    static std::wstring font_ = L"メイリオ";
+    static std::wstring font_;
     static float font_size_;
     static DWRITE_FONT_WEIGHT font_weight_;
     static DWRITE_FONT_STYLE font_style_;
@@ -26,10 +27,13 @@ private:
 public:
     static HRESULT CreateResources();
     static void DiscardResources();
-    //左上原点
     static void TextStart();
     static void TextEnd();
+
+    //左上原点
     static void WriteText(const WCHAR* text, float X, float Y, float Width, float Height);
+
+    //スタイル変更
     static HRESULT ChangeFont(const std::wstring font);
     static HRESULT ChangeFontSize(int size);
     static HRESULT SetFontColor(Color color);
