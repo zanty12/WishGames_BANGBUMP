264baf3996ea15de9978922db89bd7e895af655c
Cheung To Hung <54949055+zant123456@users.noreply.github.com>
2024-01-13 15:36:25 +0900

    camera独立化

--- camera.cpp
+++ camera.cpp
@@ -1,56 +1,56 @@
 #include "camera.h"
-
 #include "asset.h"
+#include "gameobject.h"
 
-Camera::Camera(Player* player) : pos_(player->GetPos()), player_(player)
+Camera::Camera(Vector2 start_pos,Vector2 map_size) : pos_(start_pos)
 {
     SetTexBack(LoadTexture(Asset::GetAsset(bg_stage1_back)));
     SetTexFront(LoadTexture(Asset::GetAsset(bg_stage1_front)));
     SetScale(screen_size_);
-    map_size_.x = player->GetMapMngr()->GetMap()->GetWidth() * GameObject::SIZE_;
-    map_size_.y = player->GetMapMngr()->GetMap()->GetHeight() * GameObject::SIZE_;
-    SetPos(Vector2(map_size_.x / 2, player->GetPos().y + screen_size_.y / 2 + GameObject::SIZE_));
-};
+    map_size_.x = map_size.x * GameObject::SIZE_;
+    map_size_.y = map_size.y * GameObject::SIZE_;
+    SetPos(Vector2(map_size_.x / 2, start_pos.y + screen_size_.y / 2 + GameObject::SIZE_));
+}
 
-void Camera::Update()
+void Camera::Update(Vector2 pos, Vector2 vel, PLAYER_STATE state)
 {
-    if (player_->GetPlayerState() != TOUCH_GROUND)
+    if (state != TOUCH_GROUND)
     {
-        target_pos_ = Vector2(GetPos().x, player_->GetPos().y);
+        target_pos_ = Vector2(GetPos().x, pos.y);
     }
     else
-    {
-        target_pos_ = Vector2(GetPos().x, player_->GetPos().y + screen_size_.y / 2 - player_->GetScale().y / 2 - GameObject::SIZE_);
+    {   //player外したからplayer_->GetScale().y / 2はGameObject::SIZE_に変更(2マス分の高さ)
+        target_pos_ = Vector2(GetPos().x, pos.y + screen_size_.y / 2 - GameObject::SIZE_ - GameObject::SIZE_);
     }
 
-    if(target_pos_.y < (screen_size_.y / 2))
+    if (target_pos_.y < (screen_size_.y / 2))
         target_pos_.y = screen_size_.y / 2;
 
     //move pos to target pos
-    if(GetPos().x != target_pos_.x)
+    if (GetPos().x != target_pos_.x)
     {
-        float vel = GetPos().x > target_pos_.x ? -player_->GetVel().x : player_->GetVel().x;
-        SetPos(Vector2(GetPos().x + vel, GetPos().y));
+        float new_vel = GetPos().x > target_pos_.x ? -vel.x : vel.x;
+        SetPos(Vector2(GetPos().x + new_vel, GetPos().y));
     }
     if (GetPos().y < target_pos_.y - 10.0f || GetPos().y > target_pos_.y + 10.0f)
     {
-        float vel;
-        if(GetPos().y > target_pos_.y)
+        float new_vel;
+        if (GetPos().y > target_pos_.y)
         {
-            if(player_->GetVel().y != 0.0f)
-                vel = -abs(player_->GetVel().y);
+            if (vel.y != 0.0f)
+                new_vel = -abs(vel.y);
             else
-                vel = -5.0f;
+                new_vel = -5.0f; //TODO: deltatime調整
         }
         else
         {
-            if(player_->GetVel().y != 0.0f)
-                vel = abs(player_->GetVel().y);
+            if (vel.y != 0.0f)
+                new_vel = abs(vel.y);
             else
-                vel = 5.0f;
+                new_vel = 5.0f;
         }
 
-        SetPos(Vector2(GetPos().x, GetPos().y + vel));
+        SetPos(Vector2(GetPos().x, GetPos().y + new_vel));
     }
     //move texture according to pos
     //get the percentage of the camera position in the map
