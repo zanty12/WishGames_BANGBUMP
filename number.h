51262e840a0767a6de0b975bdeef5c4838daf9c2
hamayamaeiichi <ahamayama13@gmail.com>
2024-01-16 03:13:30 +0900

    背景描画

--- number.h
+++ number.h
@@ -12,16 +12,14 @@ public:
 			texNo = LoadTexture("data/texture/UI/number.png");
 		}
 
-		int digit = std::log10(value);
-		if (digit == 0) digit = 1;
-		int tmp = value;
+		int digit = (int)std::log10(value) + 1;
 
 		for (int i = 0; i < digit; i++) {
 			int ones_place = value % 10;
 			float v = ones_place * 0.1f;
 			value *= 0.1f;
 
-			DrawSpriteLeftTop(texNo, pos + Vector2::Left * scl.x * i, 0.0f, scl, Color::White, Vector2(0.0f, v), Vector2(1.0f, 0.1f));
+			DrawSpriteLeftTop(texNo, pos + Vector2::Left * scl.x * 0.5f * i, 0.0f, scl, Color::White, Vector2(0.0f, v), Vector2(1.0f, 0.1f));
 		}
 	}
 };
