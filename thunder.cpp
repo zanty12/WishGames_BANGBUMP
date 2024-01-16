b37ac2ef42a53969d846fa5d1c8aaa85f84675b5
Cheung To Hung <54949055+zant123456@users.noreply.github.com>
2024-01-16 14:49:01 +0900

    細かいところは仕様待ち

--- thunder.cpp
+++ thunder.cpp
@@ -8,112 +8,161 @@
 // 
 //--------------------------------------------------------------------------------
 #include "thunder.h"
+
+#include "asset.h"
 #include"xinput.h"
 #include"lib/collider2d.h"
 
-bool Thunder::StickTrigger(Vector2 stick, Vector2 previousStick) {
-	stick.y *= -1;
-	previousStick.y *= -1;
-	float distance = stick.Distance();
-	float previousDistance = previousStick.Distance();
-
-	//Charge
-	if (responseMinStickDistance <= distance) {
-		charge_ += distance;
-		player_->SetGravityState(GRAVITY_NONE);
-		if (maxCharge_ < charge_) charge_ = maxCharge_;
-	}
-
-	//Release
-	if (distance < responseMinStickDistance && responseMinStickDistance < previousDistance &&
-		responseMinOneFrameDistance < (stick - previousStick).Distance()) {
-		return true;
-	}
-	return false;
+Thunder::~Thunder(){
+    for (auto& a : attack_)
+    {
+        if (a != nullptr)
+        {
+            delete a;
+        }
+    }
 }
 
-Vector2 Thunder::Move() {
-	Vector2 stick = Input::GetStickLeft(0);
-	Vector2 previousStick = Input::GetPreviousStickLeft(0);
-	stick.y *= -1;
-	previousStick.y *= -1;
-	float distance = stick.Distance();
 
-	if(StickTrigger(stick, previousStick))
-	{
-		move_dir_ = -previousStick.Normalize();
-		move_cd_ = move_cd_max_;
-		moving_ = true;
-	}
+bool Thunder::StickTrigger(Vector2 stick, Vector2 previousStick)
+{
+    stick.y *= -1;
+    previousStick.y *= -1;
+    float distance = stick.Distance();
+    float previousDistance = previousStick.Distance();
 
-	if (moving_){
-		Vector2 direction = move_dir_ * movePower * Time::GetDeltaTime();
-		player_->SetGravityState(GRAVITY_NONE);
-		charge_ -= maxCharge_ * Time::GetDeltaTime();
-		if (charge_ < 0.0f) {
-			charge_ = 0.0f;
-			moving_ = false;
-			player_->SetGravityState(GRAVITY_FULL);
-		}
-		return direction;
-	}
+    //Charge
+    if (responseMinStickDistance <= distance)
+    {
+        charge_ += distance;
+        player_->SetGravityState(GRAVITY_NONE);
+        if (maxCharge_ < charge_) charge_ = maxCharge_;
+    }
 
-	if(stick != Vector2::Zero)
-	{
-		player_->SetGravityState(GRAVITY_NONE);
-	}
-	return player_->GetVel();
+    //Release
+    if (distance < responseMinStickDistance && responseMinStickDistance < previousDistance &&
+        responseMinOneFrameDistance < (stick - previousStick).Distance())
+    {
+        return true;
+    }
+    return false;
 }
 
-void Thunder::Action() {
-	using namespace PHYSICS;
-	Vector2 stick = Input::GetStickRight(0);
-	Vector2 previousStick = Input::GetPreviousStickRight(0);
+Vector2 Thunder::Move()
+{
+    Vector2 stick = Input::GetStickLeft(0);
+    Vector2 previousStick = Input::GetPreviousStickLeft(0);
+    stick.y *= -1;
+    previousStick.y *= -1;
+    float distance = stick.Distance();
 
-	if (StickTrigger(stick, previousStick)) {
-		previousStick.y *= -1.0f;
-		Vector2 direction = -previousStick * charge_;
-		charge_ = 0.0f;
+    if (StickTrigger(stick, previousStick))
+    {
+        move_dir_ = -previousStick.Normalize();
+        move_cd_ = move_cd_max_;
+        moving_ = true;
+    }
 
-		for (int i = 0; i < _countof(arrows_); i++)
-			if (!arrows_[i].isVisible) arrows_[i] = { player_->GetPos(), direction, true };
-	}
+    if (moving_)
+    {
+        Vector2 direction = move_dir_ * movePower * Time::GetDeltaTime();
+        player_->SetGravityState(GRAVITY_NONE);
+        charge_ -= maxCharge_ * Time::GetDeltaTime();
+        if (charge_ < 0.0f)
+        {
+            charge_ = 0.0f;
+            moving_ = false;
+            player_->SetGravityState(GRAVITY_FULL);
+        }
+        return direction;
+    }
 
-	for (int i = 0; i < _countof(arrows_); i++) {
-		auto &arrow = arrows_[i];
-		if (!arrow.isVisible) continue;
-
-
-		auto enemies = player_->GetMapMngr()->GetEnemyMngr()->GetEnemies();
-
-		if (3000 < Vector2::Distance(player_->GetPos(), arrow.position)) {
-			arrows_[i].isVisible = false;
-			continue;
-		}
-		for (auto enemy : enemies) {
-			if (enemy) {
-				Vertex4 enemyCollider(enemy->GetPos(), 0.0f, enemy->GetScale());
-				float rad = atan2f(arrow.velocity.y, arrow.velocity.x);
-				auto attackCollider = PHYSICS::Vertex4(arrow.position, rad, arrowSize);
-				if (Collider2D::Touch(attackCollider, enemyCollider)) {
-					enemy->Die();
-					arrows_[i].isVisible = false;
-					break;
-				}
-			}
-		}
-
-		arrow.position += arrow.velocity * attackPower;
-		arrow.velocity += Vector2::Down * arrowGravity;
-	}
+    if (stick != Vector2::Zero)
+    {
+        player_->SetGravityState(GRAVITY_NONE);
+    }
+    return player_->GetVel();
 }
+
+void Thunder::Action()
+{
+    //cd
+    if (attack_cd_ > 0.0f)
+        attack_cd_ -= Time::GetDeltaTime();
+    if (attack_cd_ < 0.0f)
+        attack_cd_ = 0.0f;
+    //update attack
+    for (int i = 0;i < 5;i++)
+    {
+        if(attack_[i]!=nullptr)
+        {
+            attack_[i]->Update();
+            if (attack_[i]->GetDiscard())
+            {
+                delete attack_[i];
+                attack_[i] = nullptr;
+            }
+        }
+    }
+    using namespace PHYSICS;
+    Vector2 stick = Input::GetStickRight(0);
+    Vector2 previousStick = Input::GetPreviousStickRight(0);
+    stick.y *= -1;
+    previousStick.y *= -1;
+    float stick_distance = stick.Distance();
+
+    //charge up
+    if (stick_distance >= responseMinStickDistance)
+    {
+        attack_charge_ += 100 * Time::GetDeltaTime();
+        if (attack_charge_ >= attack_charge_max_)
+        {
+            attack_charge_ = attack_charge_max_;
+        }
+    }
+    //release
+    if (attack_charge_ > 0 && stick_distance < responseMinStickDistance && attack_cd_ <= 0.0f)
+    {
+        for (int i = 0;i < 5;i++)
+        {
+            if (attack_[i] == nullptr)
+            {
+                attack_[i]  = new ThunderAttack(this, -previousStick.Normalize(),
+                                           attack_charge_ / attack_charge_max_ * attack_vel_*Time::GetDeltaTime());
+                attack_charge_ = 0.0f;
+                attack_cd_ = 1.0f;
+                break;
+            }
+        }
+    }
+}
+
 
 void Thunder::DebugMenu()
 {
-	ImGui::Begin("Thunder");
-	ImGui::Text("charge:%f", charge_);
-	ImGui::SliderFloat("maxCharge", &maxCharge_, 0.0f, 100.0f);
-	ImGui::SliderFloat("movePower", &movePower, 0.0f, 5.0f);
-	ImGui::SliderFloat("attackPower", &attackPower, 0.0f, 10.0f);
-	ImGui::End();
+    ImGui::Begin("Thunder");
+    ImGui::Text("charge:%f", charge_);
+    ImGui::Text("attack_charge:%f", attack_charge_);
+    ImGui::SliderFloat("maxCharge", &maxCharge_, 0.0f, 100.0f);
+    ImGui::SliderFloat("movePower", &movePower, 0.0f, 5.0f);
+    ImGui::End();
+}
+
+ThunderAttack::ThunderAttack(Thunder* parent, Vector2 dir, float vel): parent_(parent),
+                                                                       MovableObj(parent->GetPlayer()->GetPos(),
+                                                                           atan2(-dir.y, dir.x),
+                                                                           LoadTexture(Asset::GetAsset(fire_attack)),
+                                                                           dir * vel)
+{
+    start_pos_ = parent_->GetPlayer()->GetPos();
+    SetPos(parent_->GetPlayer()->GetPos());
+    SetScale(size_);
+    SetType(OBJ_ATTACK);
+}
+
+void ThunderAttack::Update()
+{
+    if ((GetPos() - start_pos_).Distance() > 20 * GameObject::SIZE_)
+        Discard();
+    AddVel(GetVel());
 }
