d76cb7146861a20fa67482a1b52b94ad8a575736
zanty12 <54949055+zanty12@users.noreply.github.com>
2024-01-16 14:50:00 +0900

    細かいところは仕様待ち (#133)
    Co-authored-by: Cheung To Hung <54949055+zant123456@users.noreply.github.com>

--- thunder.h
+++ thunder.h
@@ -11,6 +11,7 @@
 #include "attribute.h"
 #include "lib/list.h"
 
+class ThunderAttack;
 class Thunder : public Attribute
 {
     struct Arrow {
@@ -21,29 +22,42 @@ class Thunder : public Attribute
 
     const float responseMinStickDistance = 0.5f;    // スティック傾けたとき判定する距離
     //const float maxCharge = 100.0f;                  // チャージの最大値
-    const float arrowGravity = 0.5f;                // 矢の重力
-    const Vector2 arrowSize = Vector2(100, 10);     // 矢のサイズ→向きね
+
     //const float movePower = 1;                      // 移動の力
-    //const float attackPower = 5;                    // 矢の移動の力
     float charge_ = 0.0f;                            // チャージの値
     float responseMinOneFrameDistance = 0.2f;       // 1フレームでのスティック移動量（弾くときの判定値）
-    Arrow arrows_[3];
     float move_cd_ = 1.0f;
     const float move_cd_max_ = 1.0f;
     Vector2 move_dir_;
     bool moving_ = false;
+    //attack
+    const float attack_charge_max_ = 400.0f;
+    float attack_charge_ = 0.0f;
+    float attack_cd_ = 0.0f;
+    const float attack_vel_ = 10*GameObject::SIZE_;
+    ThunderAttack* attack_[5] = {nullptr};
 
     //ここからは調整用のためconst抜き
     float maxCharge_ = 100.0f;
     float movePower = 11 * GameObject::SIZE_;
-    float attackPower = 5;
 
 public:
     Thunder(Player* player) : Attribute(player, ATTRIBUTE_TYPE_THUNDER) {}
-    ~Thunder() override = default;
+    ~Thunder()override ;
     bool StickTrigger(Vector2 stick, Vector2 previousStick);
     Vector2 Move() override;
     void Action() override;
     void DebugMenu() override;
 };
 
+class ThunderAttack : public MovableObj
+{
+    Thunder* parent_;
+    Vector2 size_ = Vector2(2 * GameObject::SIZE_, 2 * GameObject::SIZE_);
+    Vector2 start_pos_;
+public:
+    ThunderAttack() = delete;
+    ThunderAttack(Thunder* parent,Vector2 dir,float vel);
+    ~ThunderAttack() override = default;
+    void Update() override;
+};
