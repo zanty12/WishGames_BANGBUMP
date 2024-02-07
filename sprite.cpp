a7c5b6de3157f7f01ad8185c9f0ba0d0438fb5d0
hamayamaeiichi <ahamayama13@gmail.com>
2024-02-08 03:58:10 +0900

    更新

--- sprite.cpp
+++ sprite.cpp
@@ -69,16 +69,16 @@ void DrawSpriteBoxEffectLeftToRight(int texNo, Vector2 pos, Vector2 scale, Color
 
 	float len = MATH::Leap(-scale.x, +scale.x, ratio);
 	Vector2 vertices[] = {
-		Vector2(-scale.x,	+scale.y) + pos,
-		Vector2(+len	,	+scale.y) + pos,
-		Vector2(-scale.x,	-scale.y) + pos,
 		Vector2(+len	,	-scale.y) + pos,
+		Vector2(-scale.x,	-scale.y) + pos,
+		Vector2(+scale.x,	+scale.y) + pos,
+		Vector2(-len	,	+scale.y) + pos,
 	};
 	Vector2 uvs[] = {
-		Vector2(0.0f	,	0.0f),
-		Vector2(ratio	,	0.0f),
-		Vector2(0.0f	,	1.0f),
-		Vector2(ratio	,	1.0f),
+		Vector2(0.0f		,	1.0f),
+		Vector2(1.0f - ratio,	1.0f),
+		Vector2(0.0f		,	1.0f),
+		Vector2(1.0f - ratio,	1.0f),
 	};
 
 
@@ -95,10 +95,10 @@ void DrawSpriteBoxEffectBottomToUp(int texNo, Vector2 pos, Vector2 scale, Color 
 		Vector2(-scale.x,	+scale.y) + pos,
 	};
 	Vector2 uvs[] = {
-		Vector2(0.0f	,	1.0f - ratio),
-		Vector2(1.0f	,	1.0f - ratio),
-		Vector2(0.0f	,	1.0f),
-		Vector2(1.0f	,	1.0f),
+		Vector2(0.0f		,	1.0f - ratio),
+		Vector2(1.0f		,	1.0f - ratio),
+		Vector2(0.0f		,	1.0f),
+		Vector2(1.0f		,	1.0f),
 	};
 
 
