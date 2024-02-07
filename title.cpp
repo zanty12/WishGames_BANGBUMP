6a1e54faecb0650f30a374b5b88dc97ef7ca9f14
zant123456 <davidcth123456@gmail.com>
2024-02-07 17:44:17 +0900

    タイトル初期版

--- title.cpp
+++ title.cpp
@@ -3,43 +3,70 @@
 #include "asset.h"
 #include "sound.h"
 
-static int	bg_tex_ = 0;
-static int g_BGM = 0;
-
 Title::Title(SceneMngr* scene_mngr)
-	:scene_mngr_(scene_mngr)
+    : scene_mngr_(scene_mngr)
 {
-	bg_tex_ = LoadTexture((char*)"data/TEXTURE/title.webp");
-	g_BGM = LoadSound(Asset::GetAsset(SE_1).c_str());
-	PlaySound(g_BGM, -1);
+    title_video_ = new Video("data/video/title.mp4");
+    logo_tex_ = LoadTexture("data/texture/UI/team_logo.png");
+    title_video_->SetLoop(true);
+    title_video_->SetWindowPos(Vector2(Graphical::GetWidth() / 2, Graphical::GetHeight() / 2));
+    title_video_->SetSize(Vector2(Graphical::GetWidth(), Graphical::GetHeight()));
 }
 
 Title::~Title()
 {
-	StopSoundAll();
+    StopSoundAll();
 }
 
 void Title::Update()
 {
-	if (Input::GetKeyDown(0, Input::North))	//Aボタン
-	{
-		scene_mngr_->ChangeScene(SCENE_MENU);
-	}
+    //if logo
+    if (logo_)
+    {
+        logo_time_ -= Time::GetDeltaTime();
+        if(logo_alpha_ < 1.0f)
+            logo_alpha_ += Time::GetDeltaTime() / 2.0f;
+        if(logo_alpha_ > 1.0f)
+            logo_alpha_ = 1.0f;
+        if (logo_time_ <= 0.0f)
+        {
+            logo_ = false;
+        }
+    }
+    else
+    {
+        title_video_->Update();
+    }
+
+
+    if (Input::GetKeyDown(0, Input::A)) //Aボタン
+    {
+        scene_mngr_->ChangeScene(SCENE_MENU);
+    }
 }
 
 void Title::Draw()
 {
-	const float scale_x = static_cast<float>(Graphical::GetWidth()) / 1920;
-	const float scale_y = static_cast<float>(Graphical::GetHeight()) / 1080;
-	DrawSprite(bg_tex_, Vector2(Graphical::GetWidth()/2, Graphical::GetHeight()/2), 0.0f, Vector2(1920.0f*scale_x, 1080.0f*scale_y), Color(1.0f, 1.0f, 1.0f, 1.0f));
+    if (logo_)
+    {
+        const float scale_x = static_cast<float>(Graphical::GetWidth()) / 1920;
+        const float scale_y = static_cast<float>(Graphical::GetHeight()) / 1080;
+        Graphical::Clear(Color::White);
+        DrawSprite(logo_tex_, Vector2(Graphical::GetWidth() / 2, Graphical::GetHeight() / 2), 0.0f,
+                   Vector2(1088 * scale_x, 177 * scale_y), Color(1.0f,1.0f,1.0f,logo_alpha_));
+    }
+    else
+    {
+        title_video_->DrawAsResource();
+    }
 }
 
 void Title::DebugMenu()
 {
-	ImGui::Begin(u8"タイトル");
-	if(ImGui::Button(u8"ゲームスタート"))
-	{
-		scene_mngr_->ChangeScene(SCENE_MENU);
-	}
-	ImGui::End();
+    ImGui::Begin(u8"タイトル");
+    if (ImGui::Button(u8"ゲームスタート"))
+    {
+        scene_mngr_->ChangeScene(SCENE_MENU);
+    }
+    ImGui::End();
 }
