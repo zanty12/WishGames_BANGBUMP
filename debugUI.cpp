ffdf128ae618fd6f71c1561e770fd27e5c633768
Cheung To Hung <54949055+zant123456@users.noreply.github.com>
2024-02-08 15:18:31 +0900

    鯖選択画面

--- debugUI.cpp
+++ debugUI.cpp
@@ -36,7 +36,7 @@ void DebugUI::Initialize()
 	ImGui::StyleColorsDark();
 	// ファイルから日本語のフォントを追加
 	io.Fonts->AddFontFromFileTTF("data/TEXTURE/NotoSansJP-Bold.ttf", 20.0f, nullptr, io.Fonts->GetGlyphRangesJapanese());
-	io.Fonts->AddFontFromFileTTF("data/TEXTURE/JF-Dot-Kappa20-0213.ttf", 140.0f, nullptr);
+	io.Fonts->AddFontFromFileTTF("data/TEXTURE/JF-Dot-Kappa20-0213.ttf", 135.0f, nullptr);
 	// マウスカーソルの描画を有効にする
 	io.MouseDrawCursor = true;
 }
--- multi_server_select.h
+++ multi_server_select.h
@@ -7,18 +7,27 @@
 class Multi_Server_Select : public Scene
 {
 private:
-    int bg_tex_;
     char ip_[16] = "";
     bool connect_ = false;
     bool start_ = false;
     SceneMngr* scene_mngr_;
+    Video* bg_video_ = nullptr;
+    int ip_text_tex_;
+    int ip_box_tex_;
+    int scene_tex_;
 
     ImFont* font_;
 
 public:
     Multi_Server_Select(SceneMngr* scene_mngr) : scene_mngr_(scene_mngr)
     {
-        bg_tex_ = LoadTexture("data/texture/UI/multi_server_select.png");
+        ip_text_tex_ = LoadTexture("data/texture/UI/IP_text.png");
+        ip_box_tex_ = LoadTexture("data/texture/UI/IP_No.png");
+        scene_tex_ = LoadTexture("data/texture/UI/UI_serverselect.png");
+        bg_video_ = new Video("data/video/BG_serverselect.mp4");
+        bg_video_->SetLoop(true);
+        bg_video_->SetWindowPos(Vector2(Graphical::GetWidth() / 2, Graphical::GetHeight() / 2));
+        bg_video_->SetSize(Vector2(Graphical::GetWidth(), Graphical::GetHeight()));
         font_ = ImGui::GetIO().Fonts->Fonts[1];
     }
 
@@ -54,7 +63,7 @@ public:
         }
         if (start_)
             scene_mngr_->ChangeScene(SCENE_MENU);
-        if (Input::GetKeyDown(0, Input::A))
+        if (Input::GetKeyDown(0, Input::A) || GetKeyState(VK_RETURN) & 0x8000)
         {
             connect_ = true;
         }
@@ -62,23 +71,30 @@ public:
         {
             scene_mngr_->ChangeScene(SCENE_MENU);
         }
+        if (!start_ && !connect_)
+            bg_video_->Update();
     }
 
     void Draw() override
     {
         //background
-        DrawSprite(bg_tex_, Vector2(Graphical::GetWidth() / 2, Graphical::GetHeight() / 2), 0,
-                   Vector2(Graphical::GetWidth(), Graphical::GetHeight()), Color::White);
+        bg_video_->DrawAsResource();
         ImGuiWindowFlags window_flags = 0;
         window_flags |= ImGuiWindowFlags_NoBackground;
         window_flags |= ImGuiWindowFlags_NoTitleBar;
         window_flags |= ImGuiWindowFlags_NoResize;
         bool open = true;
-        float textbox_width = 140 * 7.7;
-        ImGui::SetNextWindowPos(ImVec2((Graphical::GetWidth() - textbox_width) / 2, Graphical::GetHeight() / 2 - 100));
-        //ImGui::SetNextWindowSize(ImVec2(Graphical::GetWidth() / 2, Graphical::GetHeight() / 4));
+        float textbox_width = 1070; //arbitrary value adjusted to fit all characters into a single line
+        DrawSprite(ip_text_tex_, Vector2(Graphical::GetWidth() / 2, Graphical::GetHeight() * 2 / 3), 0.0f,
+                   Vector2(1200, 300), Color(1.0f, 1.0f, 1.0f, 1.0f));
+        DrawSprite(ip_box_tex_, Vector2(Graphical::GetWidth() / 2, Graphical::GetHeight() / 2), 0.0f,
+                   Vector2(1200, 180), Color(1.0f, 1.0f, 1.0f, 1.0f));
+        DrawSprite(scene_tex_, Vector2(Graphical::GetWidth() / 2, Graphical::GetHeight() -70), 0.0f, // arbirary value of the texture
+                   Vector2(650, 650), Color(1.0f, 1.0f, 1.0f, 1.0f));
+        ImGui::SetNextWindowPos(ImVec2((Graphical::GetWidth() - textbox_width) / 2, Graphical::GetHeight() / 2 - 100)); //arbitrary value to fit text in to the textbox texture
         // Draw the texture with ImGui
         ImGui::PushFont(font_);
+        ImGui::PushStyleColor(ImGuiCol_FrameBg, ImVec4(0, 0, 0, 0));
         ImGui::Begin(u8"Ú‘±", &open, window_flags);
         ImGui::PushItemWidth(textbox_width);
         ImGui::InputText("##", ip_, 16);
@@ -90,6 +106,7 @@ public:
         }*/
         //ImGui::GetFont()->Scale /=2;
         ImGui::PopFont();
+        ImGui::PopStyleColor();
         ImGui::End();
     }
 
