6fa0197b3d42f49567e2b5e33b67eae1963e6acb
Cheung To Hung <54949055+zant123456@users.noreply.github.com>
2024-02-08 23:12:16 +0900

    マルチまーじ

--- load.h
+++ load.h
@@ -1,6 +1,56 @@
 #pragma once
 #include <thread>
 
+#include "graphical.h"
+#include "video.h"
+
+class Load
+{
+private:
+	static Video* video_;
+	static int loading_tex_;
+public:
+	Load()
+	{
+		loading_tex_ = LoadTexture("data/texture/UI/loading.png");
+	}
+	~Load()
+	{
+		delete video_;
+	}
+
+	static void LoadStart()
+	{
+		NewVideo();
+	}
+
+	static void LoadStop()
+	{
+
+	}
+
+	static void Update(void)
+	{
+		Time::Update();
+		video_->Update();
+	}
+
+	static void Draw()
+	{
+		Graphical::Clear(Color::White);
+		video_->DrawAsResource();
+		Graphical::Present();
+	}
+private:
+	static void NewVideo()
+	{
+		video_ = new Video("data/video/LoadScreen.mp4");
+		video_->SetLoop(true);
+		video_->SetWindowPos(Vector2(Graphical::GetWidth() /2 , Graphical::GetHeight() / 2));
+		video_->SetSize(Vector2(Graphical::GetWidth(), Graphical::GetHeight()));
+	}
+};
+
 class LoadClientSide {
 public:
 	static bool isNowLoad;
