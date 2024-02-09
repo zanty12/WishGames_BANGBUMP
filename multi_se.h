d3d18d4a6e9280cedc2c4a8f0249e671e85beef6
hamayamaeiichi <ahamayama13@gmail.com>
2024-02-09 11:20:12 +0900

    雷修正

--- multi_se.h
+++ multi_se.h
@@ -5,19 +5,6 @@
 
 #define SOUND_RANGE (1000.0f)
 
-void PlaySE(int seNo, Vector2 player, Vector2 other) {
-	float distanceSq = (player - other).DistanceSq();
-	float maxDistanceSq = SOUND_RANGE * SOUND_RANGE;
-	if (distanceSq < maxDistanceSq) {
-		float rate = distanceSq / maxDistanceSq;
-		float volume = MATH::Leap(0.3f, 0.01f, rate);
-		PlaySound(seNo, false);
-		SetVolume(seNo, volume);
-	}
-}
+void PlaySE(int seNo, Vector2 player, Vector2 other);
 
-void PlaySE(int seNo) {
-	float volume = 0.3f;
-	PlaySound(seNo, false);
-	SetVolume(seNo, volume);
-}
+void PlaySE(int seNo);
