f34fd94bbe5b8d5d10d336003b87684138a55eb3
hamayamaeiichi <ahamayama13@gmail.com>
2024-02-28 00:15:55 +0900

    剣が取れないエラー修正2

--- multi_character_select_frame.cpp
+++ multi_character_select_frame.cpp
@@ -34,7 +34,7 @@ void CharacterSelectFrameClientSide::Draw(int frameTexNo, int framePTexNo, int a
 
 	// 割合からアニメーションを決める（ソード編）
 	float swordRate = stateSmooth -1.0f;
-	if (swordRate <= 0.0f) swordAnim.MoveBegin();
+	if (swordRate <= 0.0f || stateSmooth.get_tilt() < 0.0f) swordAnim.MoveBegin();
 	std::cout << stateSmooth << std::endl;
 
 
